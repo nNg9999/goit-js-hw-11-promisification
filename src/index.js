d6eda9dc98c6b68c1e92844cdff08e409abfc9f7
nNg9999 <n_n_g_@ukr.net>
2020-04-19 00:35:04 +0300

    added hw11

---
+++ src/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import './css/styles.css';
+
+// якщо однакові стилі сss в різних файлах , то включаємо в конфігурації тип module: true
+// import styles from './css/styles.css';
+
+import './js/delay';
+import './js/makeTransaction';
+import './js/toggleUserState';
